677cb2fa28e9af1be35203bc1425b0044224b181
Abhishek Madhukar Gholap <gholapabhishek9@gmail.com>
2023-08-01 16:09:29 +0530

    simplyfy

---
+++ app/src/main/java/com/android/ranit/contactsentrepot/contract/IMainActivityContract.java
@@ -0,0 +1,43 @@
+package com.android.ranit.contactsentrepot.contract;
+
+import android.net.Uri;
+
+import com.airbnb.lottie.LottieAnimationView;
+import com.android.ranit.contactsentrepot.data.ContactResponse;
+
+import java.util.List;
+
+/**
+ * Contract to be implemented by MainActivity (View) and MainActivityViewModel (ViewModel)
+ *
+ * Created by: Ranit Raj Ganguly on 15/04/21.
+ */
+public interface IMainActivityContract {
+
+    // View
+    interface View {
+        void initializeViews();
+        void setupLottieAnimation(LottieAnimationView animationView, String animationName);
+        void setupHandlerThreads();
+        void destroyHandlerThreads();
+        void onImportContactButtonClicked();
+        void onExportIntoExcelButtonClicked();
+        void onReadFromExcelButtonClicked();
+        void onShareButtonClicked();
+        void switchVisibility(android.view.View view, int visibility);
+        void enableUIComponent(android.view.View componentName);
+        void disableUIComponent(android.view.View componentName);
+        void setupRecyclerView();
+        void displaySnackBar(String message);
+        boolean checkPermissionsAtRuntime();
+        void requestPermissions();
+    }
+
+    // View-Model
+    interface ViewModel {
+        void initiateImport();
+        void initiateExport(List<ContactResponse> dataList);
+        void initiateRead();
+        Uri initiateSharing();
+    }
+}
